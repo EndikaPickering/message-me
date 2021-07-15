32a4dfa99d0e69acc5d8f97a34ee479c72107601
Endika <52050393+EndikaPickering@users.noreply.github.com>
2021-07-15 12:54:37 +0100

    Add user table

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "joebloggs",password: "password")
+User.create(username: "johndoe",password: "password")
+User.create(username: "janedoe",password: "password")
+User.create(username: "jackdoe",password: "password")
+User.create(username: "jackjack",password: "password")
